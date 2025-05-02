7c7e58f7f8933509ebe92e519cd449aba1b9adc3
minho <deforto42@gmail.com>
2025-05-02 10:04:34 +0900

    240502

---
+++ FastOrder/패스트오더.sql
@@ -0,0 +1,7 @@
+-- 관리자 계정 생성
+INSERT INTO fast.users (created_at, updated_at, created_by, updated_by, description, is_deleted, level, login_id, login_password, name, store_group_id)
+VALUES (NOW(), null, 10000000, null, null, false, 'MASTER'
+        , 'hyun', '$2a$10$nW3zeVCeXWBwEJ9OmQLey.q9Zp82Oe9YrAQuwDbPGCdDxQfyuQPnK'
+        , '현성규', null);
+
+SELECT * FROM users WHERE level = 'MASTER';
