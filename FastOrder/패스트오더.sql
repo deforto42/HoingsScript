fef58d22fc0c158b6ef3c8c78aff9d6a2eb53166
minho <deforto42@gmail.com>
2025-05-02 17:09:11 +0900

    250502

--- FastOrder/패스트오더.sql
+++ FastOrder/패스트오더.sql
@@ -4,4 +4,4 @@ VALUES (NOW(), null, 10000000, null, null, false, 'MASTER'
         , 'hyun', '$2a$10$nW3zeVCeXWBwEJ9OmQLey.q9Zp82Oe9YrAQuwDbPGCdDxQfyuQPnK'
         , '현성규', null);
 
-SELECT * FROM users WHERE level = 'MASTER';
+SELECT * FROM users WHERE level = 'MASTER' AND login_password = '$2a$10$nW3zeVCeXWBwEJ9OmQLey.q9Zp82Oe9YrAQuwDbPGCdDxQfyuQPnK';
