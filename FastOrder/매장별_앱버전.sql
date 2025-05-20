a3da87c06784478e7c63a6b681272e547f14d963
minho <deforto42@gmail.com>
2025-05-20 17:55:48 +0900

    -

--- FastOrder/매장별_앱버전.sql
+++ FastOrder/매장별_앱버전.sql
@@ -11,9 +11,26 @@ FROM (
         WHERE t.app_version IS NOT NULL
         AND t.device_id IS NOT NULL
         AND t.device_push_token IS NOT NULL
+        AND s.name NOT LIKE '%테스트%'
+        AND s.name NOT LIKE '%이관전%'
+        AND s.name NOT LIKE '%영업%'
+        AND s.name NOT LIKE '%사무실%'
+        AND s.name NOT LIKE '%폐업%'
+        AND s.name NOT LIKE '%쇼룸%'
+        AND s.name NOT LIKE '%쇼륨%'
+        AND s.name NOT LIKE '%본사%'
+        AND s.name NOT LIKE '%시안%'
+        AND s.name NOT LIKE '%시연%'
+        AND s.name NOT LIKE '%웹 연동%'
+        AND s.name NOT LIKE '%QA%'
+        AND EXISTS (SELECT 1
+                      FROM orders
+                      WHERE total_price != 0
+                      AND store_id = s.id)
         GROUP BY s.id
                 , s.name
                 , t.app_version
      ) z
 GROUP BY z.id
-       , z.name;
+       , z.name
+ORDER BY appVersion;
--- FastOrder/패스트오더.sql
+++ FastOrder/패스트오더.sql
@@ -1,7 +1,7 @@
 -- 관리자 계정 생성
 INSERT INTO fast.users (created_at, updated_at, created_by, updated_by, description, is_deleted, level, login_id, login_password, name, store_group_id)
 VALUES (NOW(), null, 10000000, null, null, false, 'MASTER'
-        , 'brjeon', '$2a$10$nW3zeVCeXWBwEJ9OmQLey.q9Zp82Oe9YrAQuwDbPGCdDxQfyuQPnK'
-        , '전병래', null);
+        , 'api_token_foodtech', '$2a$10$nW3zeVCeXWBwEJ9OmQLey.q9Zp82Oe9YrAQuwDbPGCdDxQfyuQPnK'
+        , 'API토큰_푸드테크', null);
 
 SELECT * FROM users WHERE level = 'MASTER' AND login_password = '$2a$10$nW3zeVCeXWBwEJ9OmQLey.q9Zp82Oe9YrAQuwDbPGCdDxQfyuQPnK';
